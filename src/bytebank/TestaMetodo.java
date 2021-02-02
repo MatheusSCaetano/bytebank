9a9934337ff297a84f18870d9fca7d9912344ea5
MatheusSCaetano <matheuscaetano260401@gmail.com>
2021-02-01 23:27:14 -0300

    Definindo comportamento

--- src/bytebank/TestaMetodo.java
+++ src/bytebank/TestaMetodo.java
@@ -6,5 +6,20 @@ public static void main(String[] args) {
 	 conta.saldo=100;
 	 conta.deposita(50);
 	 System.out.println("Saldo: "+conta.saldo);
+	 
+	 boolean deucerto = conta.saca(20);//posso instanciar sem a variavel para receber o retorno
+	 System.out.println(conta.saldo);
+	 System.out.println(deucerto);
+	 
+	 Conta contaDestinatario = new Conta();
+	 contaDestinatario.deposita(1000);
+	 boolean sucessotranfer = contaDestinatario.transfere(5000, conta);
+	 if(sucessotranfer) {
+		 System.out.println("Tranferencia realizada com sucesso!");
+	 }else {
+		 System.out.println("Você não possui dinheiro suficiente!");
+	 }
+	 System.out.println("Conta do destatario despois de transferir 300 reais: "+contaDestinatario.saldo);
+	 System.out.println("Conta que recebeu os 300 reais: "+conta.saldo);
 }
 }
