943a8e2d39fe157ec8a4a09f31ec5c19e76a3afb
MatheusSCaetano <matheuscaetano260401@gmail.com>
2021-02-05 00:04:35 -0300

    Testando Get e Set de referência

--- src/bytebank/TestaGetESet.java
+++ src/bytebank/TestaGetESet.java
@@ -6,6 +6,17 @@ public static void main(String[] args) {
 	Conta conta = new Conta();
 	conta.setNumero(1337);//dentro do set posso realizar validações
 	System.out.println(conta.getNumero());
+	
+	Cliente matheus = new Cliente();
+	//conta.titula=paulo
+	matheus.setNome("Teste nome");
+	
+    conta.setTitular(matheus);
+    System.out.println("Nome do titular: "+conta.getTitular().getNome());
+    conta.getTitular().setNome("Matheus 2");
+    conta.getTitular().setProfissao("Programador");
+    System.out.println("Nome do titular: "+conta.getTitular().getNome());
+    System.out.println("Profissão do titular: "+conta.getTitular().getProfissao());
 }
 
 }
